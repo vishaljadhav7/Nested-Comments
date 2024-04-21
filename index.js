04ebc315b52ebc74f7eb1f64320adf907b01aff6
Vishal Jadhav <143590501+vishaljadhav7@users.noreply.github.com>
2024-04-21 17:20:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
-let commentContainer = document.getElementById("comment-container");
 
-function createInputBox() {
-  let div = document.createElement("div");
-  div.setAttribute("class", "comment-details");
 
-  div.innerHTML += `
-    <input type="text" placeholder="add text here" class="input" />
-    <button class="btn submit">Submit</button>`;
 
-  return div;
+const container = document.querySelector('.comments-container');
+
+
+function addInputBox(){
+  const inputBox = document.createElement('div')
+  inputBox.setAttribute('class', 'comment-details');
+  
+   inputBox.innerHTML = ` 
+   <input type="text" id="" class="comment-input" />
+   <button type="submit" class="comment-submit">submit</button>
+   ` 
+
+  return inputBox ;
+  
 }
 
-function addReply(text) {
-  let div = document.createElement("div");
-  div.setAttribute("class", "all-comment");
 
-  div.innerHTML += `
-    <div class="card">
-      <span class="text">${text}</span>
-      <span id="reply" class="reply">
-        Add Reply
-      </span>
-    </div>`;
+function addComment(value){
+  const commentDiv = document.createElement('div');
+  commentDiv.setAttribute('class','all-comments')
 
-  return div;
+  commentDiv.innerHTML = `<div class="card">
+  <span class="comment">${value}</span>
+  <span class="reply">add reply</span>
+  </div>`
+
+  return commentDiv;
+
 }
 
-commentContainer.addEventListener("click", function (e) {
-  let replyClicked = e.target.classList.contains("reply");
-  let submitClicked = e.target.classList.contains("submit");
-  let closestCard = e.target.closest(".all-comment");
+container.addEventListener('click', function handleClick(e){
+    const isReply = e.target.classList.contains("reply");
+    const isSubmit = e.target.classList.contains("comment-submit");
+    const closestCard = e.target.closest(".all-comments");
+    
+    
+    if(isReply){
+      // add input box
+      closestCard.appendChild(addInputBox());
+    }else if(isSubmit){
+      // add comment
+      const commentDetails = e.target.closest('.comment-details');
+      const commentData = commentDetails.querySelector('.comment-input'); 
+      if(commentData.value){
+        closestCard.appendChild(addComment(commentData.value))
+        commentDetails .remove()
+      }
 
-  if (replyClicked) {
-    closestCard.appendChild(createInputBox());
-  }
-
-  if (submitClicked) {
-    const commentDetails = e.target.closest(".comment-details");
-    if (commentDetails.children[0].value) {
-      closestCard.appendChild(addReply(commentDetails.children[0].value));
-      commentDetails.remove();
     }
-  }
-});
+  })
+
